71037d8ebfdf7e673395626c79cb2168ccbe3cb7
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-03-23 15:13:08 +0000

    Exercises 1.3.-1.5 - Remove component nesting!!!

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  const Header = props => <h1>{props.course.name}</h1>;
-  const Content = props => (
-    <>
-      {props.parts.map(coursePart => (
-        <Part coursePart={coursePart} />
-      ))}
-    </>
-  );
-  const Part = props => (
-    <p>
-      {props.coursePart.name} {props.coursePart.exercises}
-    </p>
+const Header = props => <h1>{props.course.name}</h1>;
+
+const Content = props => (
+  <>
+    {props.parts.map(coursePart => (
+      <Part coursePart={coursePart} />
+    ))}
+  </>
+);
+
+const Part = props => (
+  <p>
+    {props.coursePart.name} {props.coursePart.exercises}
+  </p>
+);
+
+const Total = props => {
+  const accumulatedNumbersOfExercises = props.parts.map(
+    coursePart => coursePart.exercises
   );
 
+  const totalNumberOfExercises = accumulatedNumbersOfExercises.reduce(
+    (exercise1, exercise2) => exercise1 + exercise2,
+    0
+  );
+
+  return <p>Number of exercises {totalNumberOfExercises}</p>;
+};
+
+const App = () => {
   const course = {
     name: 'Half Stack application development',
     parts: [
@@ -32,19 +47,6 @@ const App = () => {
         exercises: 14
       }
     ]
-  };
-
-  const Total = props => {
-    const accumulatedNumbersOfExercises = props.parts.map(
-      coursePart => coursePart.exercises
-    );
-
-    const totalNumberOfExercises = accumulatedNumbersOfExercises.reduce(
-      (exercise1, exercise2) => exercise1 + exercise2,
-      0
-    );
-
-    return <p>Number of exercises {totalNumberOfExercises}</p>;
   };
 
   return (
