2d1057cf754a6f9c6ea62add0ec3362e1467e9c0
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-04-15 14:50:05 +0100

    2.8: The Phonebook Step3
    - Application has been extended to allow persisting of phone numbers

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -3,20 +3,23 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' },
   ]);
-  const [newName, setNewName] = useState('');
+  const [newPerson, setNewPerson] = useState({
+    name: '',
+    number: '',
+  });
 
   const addPerson = (event) => {
     event.preventDefault();
 
-    const names = persons.map(person => person.name);
-    if (names.includes(newName)) {
-      alert(`${newName} is already added to the phonebook.`);
+    const names = persons.map((person) => person.name);
+    if (names.includes(newPerson.name)) {
+      alert(`${newPerson.name} is already added to the phonebook.`);
       return;
     }
 
-    setPersons(persons => [...persons, { name: newName }])
+    setPersons((persons) => [...persons, newPerson]);
   };
 
   return (
@@ -25,19 +28,44 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           <label>Name: </label>
-          <input onChange={(event) => setNewName(event.target.value)} value={newName} />
+          <input
+            onChange={(event) =>
+              setNewPerson({
+                ...newPerson,
+                name: event.target.value,
+              })
+            }
+            value={newPerson.name}
+          />
+
+          <br />
+
+          <label>Number: </label>
+          <input
+            onChange={(event) =>
+              setNewPerson({
+                ...newPerson,
+                number: event.target.value,
+              })
+            }
+            value={newPerson.number}
+          />
         </div>
         <div>
-          <button type="submit">Add</button>
+          <button type='submit'>Add</button>
         </div>
       </form>
 
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+        {persons.map((person) => (
+          <li key={person.name}>
+            {person.name}: {person.number}
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
