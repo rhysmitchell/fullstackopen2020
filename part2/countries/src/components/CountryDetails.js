70fdbdfde68f4590a6ed74df4c804595e944665b
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-05-04 12:01:25 +0100

    2.14*: Data for countries, step3

--- part2/countries/src/components/CountryDetails.js
+++ part2/countries/src/components/CountryDetails.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const CountryDetails = (props) => {
   const { countries, countryToShow } = props;
@@ -12,31 +13,54 @@ const CountryDetails = (props) => {
   return <></>;
 };
 
-const SingleCountry = ({ country }) => (
-  <>
-    <h2>{country.name}</h2>
-    <ul style={{ listStyle: 'none', paddingLeft: 0 }}>
-      <li key={country.capital}>
-        <span>Capital: {country.capital}</span>
-      </li>
-      <li key={country.population}>
-        <span>Population: {country.population}</span>
-      </li>
-    </ul>
+const SingleCountry = ({ country }) => {
+  const [weatherDetails, setWeatherDetails] = useState({});
 
-    <h2>Languages</h2>
-    <ul style={{ listStyle: 'none', paddingLeft: 0 }}>
-      {country.languages.map((language) => (
-        <li key={language.name}>{language.name}</li>
-      ))}
-    </ul>
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.name}`
+      )
+      .then((response) => {
+        setWeatherDetails(response.data.current);
+      });
+  }, []);
 
-    <img
-      src={country.flag}
-      alt={country.name}
-      style={{ maxWidth: '200px', maxHeight: 'auto' }}
-    />
-  </>
-);
+  return (
+    <>
+      <h2>{country.name}</h2>
+      <ul style={{ listStyle: 'none', paddingLeft: 0 }}>
+        <li key={country.capital}>
+          <span>Capital: {country.capital}</span>
+        </li>
+        <li key={country.population}>
+          <span>Population: {country.population}</span>
+        </li>
+      </ul>
+      <h2>Languages</h2>
+      <ul style={{ listStyle: 'none', paddingLeft: 0 }}>
+        {country.languages.map((language) => (
+          <li key={language.name}>{language.name}</li>
+        ))}
+      </ul>
+      <img
+        src={country.flag}
+        alt={country.name}
+        style={{ maxWidth: '200px', maxHeight: 'auto' }}
+      />
+      <h2 style={{ margin: 0 }}>Weather in {country.name}</h2>
+      <strong>Temperature: </strong>
+      {weatherDetails.temperature} Celcius
+      <br />
+      {weatherDetails.weather_icons &&
+        weatherDetails.weather_icons.map((icon) => (
+          <img src={icon} alt={weatherDetails.observation_time} />
+        ))}
+      <br />
+      <strong>Wind: </strong> {weatherDetails.wind_speed} mph direction{' '}
+      {weatherDetails.wind_dir}
+    </>
+  );
+};
 
 export default CountryDetails;
