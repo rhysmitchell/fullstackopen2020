18908596ec9bbff7ff346a97892bc7e61cb962d4
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-07-22 09:12:28 +0100

    uibuild

---
+++ part3/phonebook-backend/build/precache-manifest.43d41e0232dfacf8d439f8710e87dda8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256bbf2e2a4e0fb1cab4fc0dea53beb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "131626fe80a326ed1a84",
+    "url": "/static/js/2.acd2655c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.acd2655c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f012de9b4fddc51c6589",
+    "url": "/static/js/main.7635fe47.chunk.js"
+  },
+  {
+    "revision": "4caee76f79619d3bb41a",
+    "url": "/static/js/runtime-main.92e7eacc.js"
+  }
+]);
