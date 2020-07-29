decdba47993cab8e0127c1fb27c3c0f0b72bbbe7
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-07-29 17:08:45 +0100

    4.6*: helper functions and unit tests, step4

--- part4/bloglist/tests/blogCollection.test.js
+++ part4/bloglist/tests/blogCollection.test.js
@@ -6,42 +6,48 @@ const blogs = [
     title: "React patterns",
     author: "Michael Chan",
     url: "https://reactpatterns.com/",
-    likes: 7
+    likes: 7,
+    blogs: 7
   },
   {
     id: "5a422aa71b54a676234d17f8",
     title: "Go To Statement Considered Harmful",
     author: "Edsger W. Dijkstra",
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5
+    likes: 5,
+    blogs: 5
   },
   {
     id: "5a422b3a1b54a676234d17f9",
     title: "Canonical string reduction",
     author: "Edsger W. Dijkstra",
     url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12
+    likes: 12,
+    blogs: 12
   },
   {
     id: "5a422b891b54a676234d17fa",
     title: "First class tests",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10
+    likes: 10,
+    blogs: 10
   },
   {
     id: "5a422ba71b54a676234d17fb",
     title: "TDD harms architecture",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0
+    likes: 0,
+    blogs: 0
   },
   {
     id: "5a422bc61b54a676234d17fc",
     title: "Type wars",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2
+    likes: 2,
+    blogs: 2
   }
 ]
 
@@ -59,8 +65,7 @@ describe('Total likes', () => {
       title: 'Go To Statement Considered Harmful',
       author: 'Edsger W. Dijkstra',
       url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
+      likes: 5
     }
   ]
 
@@ -74,5 +79,12 @@ describe('Blog with the most likes', () => {
   test('Top blog post', () => {
     const result = listHelper.favoriteBlog(blogs)
     expect(result).toBe(12)
+  })
+})
+
+describe('Author with the most blogs', () => {
+  test('Top blog poster', () => {
+    const result = listHelper.mostBlogs(blogs)
+    expect(result.blogs).toBe(12)
   })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,9 +6,19 @@ const totalLikes = (blogs) => blogs[0].likes;
 
 const favoriteBlog = (blogs) => Math.max.apply(Math, blogs.map(blog => blog.likes));
 
+const mostBlogs = (passedBlogs) => {
+    const { author, blogs } = passedBlogs.reduce((prev, current) => (+prev.blogs > +current.blogs) ? prev : current)
+
+    return {
+        author: author,
+        blogs: blogs
+    };
+};
+
 module.exports = {
     blogsExist,
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
