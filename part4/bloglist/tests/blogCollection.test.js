69b380951485dd1785dae6b660e44b2073ccd8e8
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-07-29 16:27:17 +0100

    4.4: helper functions and unit tests, step2

--- part4/bloglist/tests/blogCollection.test.js
+++ part4/bloglist/tests/blogCollection.test.js
@@ -1,14 +1,26 @@
 const listHelper = require('../utils/list_helper')
 
-describe('Blog collection', () => {
-  test('at least one blog exists in the collection', () => {
-    expect(listHelper.blogsExist(['Blog no. 1', 'Blog no. 2', 'Blog no. 3'])).toBe(true)
-  })
+test('Dummy returns one', () => {
+  const blogs = []
 
-  test('Dummy returns one', () => {
-    const blogs = []
-  
-    const result = listHelper.dummy(blogs)
-    expect(result).toBe(1)
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
+
+describe('Total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
   })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,7 +2,10 @@ const blogsExist = (collection) => collection.length > 0;
 
 const dummy = (blogs) => 1;
 
+const totalLikes = (array) => array[0].likes;
+
 module.exports = {
     blogsExist,
-    dummy
+    dummy,
+    totalLikes
 }
