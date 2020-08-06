edf6286630343c03a2a23086cbea955b6bde4de7
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-08-06 09:23:42 +0100

    4.9*: Blog list tests, step2

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,16 +1,20 @@
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
 
-const api = supertest(app)
+const api = supertest(app);
 
 test('blogs are returned as json', async () => {
     await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
-})
+});
 
-afterAll(() => {
-    mongoose.connection.close()
-})
+test('id property is named correctly (i.e. not _id)', async () => {
+    const response = await api.get('/api/blogs');
+    const blogs = response.body;
+    blogs.map(blog => expect(blog.id).toBeDefined());
+});
+
+afterAll(() => mongoose.connection.close());
