8e9e4daa8994d849fc3c08163f214e44550aad9d
RhysTheDev <rhys.mitchell92@googlemail.com>
2020-08-19 22:47:21 +0100

    4.9*: Blog list tests, step2
    - I've tried to implement a rule here that applies to all blog posts in the array... so if one blog post deviates then the test will fail.
       - Obviously the simpler thing would do is to take the first element and test that but I thought this was more thorough.

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,16 +1,24 @@
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
 
-const api = supertest(app)
+const api = supertest(app);
 
 test('blogs are returned as json', async () => {
     await api
         .get('/api/blogs')
         .expect(200)
-        .expect('Content-Type', /application\/json/)
-})
+        .expect('Content-Type', /application\/json/);
+});
 
-afterAll(() => {
-    mongoose.connection.close()
-})
+test('blog posts contain id property', async () => {
+    const response = await api.get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+
+    const blogs = response.body;
+    blogs.every(blog => expect(blog.id).toBeDefined());
+});
+
+afterAll(() =>
+    mongoose.connection.close());
