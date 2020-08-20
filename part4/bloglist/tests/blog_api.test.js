21f4be96f5c199ae0e1ef6760041fbcaca976e63
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-08-20 17:41:54 +0100

    4.9*: Blog list tests, step2

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -3,6 +3,31 @@ const supertest = require('supertest');
 const app = require('../app');
 
 const api = supertest(app);
+const Blog = require('../models/Blog');
+
+const initialBlogs = [
+    {
+        title: 'Boring blog pt. 1',
+        author: 'Not Rhys Mitchell',
+        url: 'www.boring-blog.com',
+        likes: 1
+    },
+    {
+        title: 'Boring blog pt. 2',
+        author: 'Not Rhys Mitchell',
+        url: 'www.boring-blog.com',
+        likes: 2
+    },
+];
+
+beforeEach(async () => {
+    await Blog.deleteMany({});
+
+    initialBlogs.forEach(async (blog) => {
+        const newBlog = new Blog(blog);
+        await newBlog.save();
+    });
+});
 
 test('blogs are returned as json', async () => {
     await api
@@ -15,6 +40,22 @@ test('id property is named correctly (i.e. not _id)', async () => {
     const response = await api.get('/api/blogs');
     const blogs = response.body;
     blogs.map(blog => expect(blog.id).toBeDefined());
+});
+
+test('blog can be created', async () => {
+    await api
+        .post('/api/blogs')
+        .send({
+            title: 'Boring blog pt. 3',
+            author: 'Not Rhys Mitchell',
+            url: 'www.boring-blog.com',
+            likes: 3
+        })
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+
+    const blogsAfterAdd = await api.get('/api/blogs');
+    expect(blogsAfterAdd.body).toHaveLength(initialBlogs.length + 1)
 });
 
 afterAll(() => mongoose.connection.close());
