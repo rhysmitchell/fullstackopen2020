60a7b39c8ca90c8b57526a77de65813b9c04b1d0
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-09-04 16:33:33 +0100

    A couple of bug fixes regarding adding blogs with users at the same time
    - Populate method has been chained to the mongo .Find({}) method
    - Added user reference to the blog schema

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 
 usersRouter.get('/', async (request, response) => {
     const users = await User
-        .find({}).populate('blogs');
+        .find({}).populate('blogs', { title: 1, author: 1, likes: 1 });
     response.json(users.map(user => user.toJSON()));
 });
 
