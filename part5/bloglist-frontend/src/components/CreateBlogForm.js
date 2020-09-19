98ca55ede3692f87bce6e99ec3373bbe4224328a
RhysTheDev <rhys.mitchell92@googlemail.com>
2020-09-19 06:53:03 +0100

    5.5 Blog list frontend, step5
    The new blog form is now togglable via a button
    I might change this to use props.children, but for now, in my mind, it's simple enough.

--- part5/bloglist-frontend/src/components/CreateBlogForm.js
+++ part5/bloglist-frontend/src/components/CreateBlogForm.js
@@ -1,43 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CreateBlogForm = ({ user, handleBlogCreation, title, setTitle, author, setAuthor, url, setUrl }) => {
+    const [createBlogVisible, setCreateBlogVisible] = useState(false)
+
     if (!user) {
         return <></>;
     }
-    
-    return (
-        <form onSubmit={handleBlogCreation}>
-            <div>
-                Title
-            <input
-                    type="text"
-                    value={title}
-                    name="Title"
-                    onChange={(e) => setTitle(e.currentTarget.value)}
-                />
-            </div>
-            <div>
-                Author
-            <input
-                    type="text"
-                    value={author}
-                    name="Author"
-                    onChange={(e) => setAuthor(e.currentTarget.value)}
-                />
-            </div>
+    if (createBlogVisible) {
+        return (
+            <>
+            <h3>Create new blog</h3>
+                <form onSubmit={handleBlogCreation}>
+                    <div>
+                        Title
+                <input
+                            type="text"
+                            value={title}
+                            name="Title"
+                            onChange={(e) => setTitle(e.currentTarget.value)}
+                        />
+                    </div>
+                    <div>
+                        Author
+                <input
+                            type="text"
+                            value={author}
+                            name="Author"
+                            onChange={(e) => setAuthor(e.currentTarget.value)}
+                        />
+                    </div>
 
-            <div>
-                Url
-            <input
-                    type="text"
-                    value={url}
-                    name="Url"
-                    onChange={(e) => setUrl(e.currentTarget.value)}
-                />
-            </div>
-            <button type="submit">Create</button>
-        </form>
-    );
+                    <div>
+                        Url
+                <input
+                            type="text"
+                            value={url}
+                            name="Url"
+                            onChange={(e) => setUrl(e.currentTarget.value)}
+                        />
+                    </div>
+                    <button type="submit">Create</button>
+                    <button onClick={() => setCreateBlogVisible(false)}>cancel</button>
+                </form>
+            </>
+        );
+    }
+    else {
+        return (<><br /><button onClick={() => setCreateBlogVisible(true)}>Create new blog</button></>)
+    }
 };
 
 export default CreateBlogForm;
