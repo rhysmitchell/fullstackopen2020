812e66abf831703b37ffd4b8aec26e97ecb0b8f2
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-09-21 10:49:28 +0100

    5.7* Blog list frontend, step7 View/hide toggle button added to Blog component. Optional chaining used to conditionally display 'Anonymous User' in the case where a user doesn't exist for a particular blog.

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Blog = ({ id, blog }) => (
-  <li key={id}>
-    {blog.title} [by {blog.author}]
-  </li>
-)
+const Blog = ({ id, blog }) => {
+  const [blogHidden, setBlogHidden] = useState(true);
+
+  return (
+    <li key={id}>
+      {blog.title} [by {blog.author}]
+      <button onClick={() => setBlogHidden(!blogHidden)}>{blogHidden ? 'Show' : 'Hide'}</button>
+      {!blogHidden &&
+        (<ul>
+          <li>{blog.url}</li>
+          <li>
+            {blog.likes}
+            <button onClick={() => console.log('Not implemented just yet...')}>Like</button>
+          </li>
+          <li>{blog?.user?.name || 'Anonymous user'}</li>
+        </ul>)
+      }
+    </li>)
+}
 
 export default Blog;
