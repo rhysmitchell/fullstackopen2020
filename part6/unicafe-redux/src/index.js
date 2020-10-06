006a769add22dab0c8ca1fcd2a43b1f594b538a3
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-10-06 07:18:40 +0100

    6.1: unicafe revisited, step1
    Tests have been written for each of the store changes.
    The functionality has been brought through to the UI

---
+++ part6/unicafe-redux/src/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+
+  const reset = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={good}>good</button>
+      <button onClick={ok}>Neutral</button>
+      <button onClick={bad}>Bad</button>
+      <button onClick={reset}>Reset Stats</button>
+      <div>Good {store.getState().good}</div>
+      <div>Neutral {store.getState().ok}</div>
+      <div>Bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
