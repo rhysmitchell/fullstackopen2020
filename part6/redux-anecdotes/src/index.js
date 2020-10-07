ee4e3b70f08ab2b003722866303fc7952075228b
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-10-07 17:08:29 +0100

    Added redux-devtools-extension for added visibility

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -4,8 +4,9 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 import reducer from './reducers/anecdoteReducer'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducer)
+const store = createStore(reducer, composeWithDevTools())
 
 ReactDOM.render(
   <Provider store={store}>
