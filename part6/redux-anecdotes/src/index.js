dc51237c55d57afa8c943356e169ba3d2730b00d
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-10-08 06:44:07 +0100

    6.9 Better anecdotes, step7

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { store } from './store'
 
-const store = createStore(reducer, composeWithDevTools())
+
 
 ReactDOM.render(
   <Provider store={store}>
