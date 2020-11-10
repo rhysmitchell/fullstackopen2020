23de49c876116b582f41776ef8f103383430705a
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-11-10 16:18:19 +0000

    8.4: Books of an author

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -97,7 +97,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -106,7 +106,13 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if(args.author) {
+        return books.filter(book => book.author === args.author)
+      }
+
+      return books
+    },
     allAuthors: () => authors,
   },
   Author: {
