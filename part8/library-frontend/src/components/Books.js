9e15b5c932e252d3dd6c9645a7857d81b5f11b3d
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-11-13 22:43:57 +0000

    8.9: Books view
    - Same treatment given to the Books
    - GraphQL logic added to retrieve books to the component for rendering

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_BOOKS = gql`
+query {
+  allBooks { 
+    title 
+    author
+    published 
+  }
+}
+
+`
 
 const Books = (props) => {
+  const books = useQuery(ALL_BOOKS)
+
   if (!props.show) {
     return null
   }
 
-  const books = []
+  if (books.loading) {
+    return <div>loading...</div>
+  }
 
   return (
     <div>
@@ -22,7 +38,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
+          {books.data.allBooks.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
