1928f40f7b10c83c3ba015401c593c91e5eef49f
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-11-13 22:37:47 +0000

    8.8: Authors view
    - Added GraphQL query to deliver results to Authors component

---
+++ part8/library-frontend/src/App.js
@@ -0,0 +1,46 @@
+
+import React, { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`
+const App = () => {
+  const authors = useQuery(ALL_AUTHORS)
+  const [page, setPage] = useState('authors')
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors
+        authors={authors}
+        show={page === 'authors'}
+      />
+
+      <Books
+        show={page === 'books'}
+      />
+
+      <NewBook
+        show={page === 'add'}
+      />
+
+    </div>
+  )
+}
+
+export default App
---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,41 @@
+
+import React from 'react'
+
+const Authors = (props) => {
+  if (!props.show) {
+    return null
+  }
+
+  if (props.authors.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {props.authors.data.allAuthors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors
