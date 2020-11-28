b3ee45d1164a58b6b4da5c195116886bef7c8c2c
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-11-28 07:12:53 +0000

    9.5 WebBMI
    - Added the actual BMI value to the 'BMI message'
    - Exported bmiCalculator and created an endpoint for it in index.ts

--- part9/calculator/bmiCalculator.ts
+++ part9/calculator/bmiCalculator.ts
@@ -1,27 +1,27 @@
 const calculateBmi = (height: number, weight: number): string => {
 
-    if(!height || !weight) {
+    if (!height || !weight) {
         throw new Error(`Height or Weight arguments weren't provided.`)
     }
 
-    const bmi: number = weight / (height / 100 * height / 100);
+    const bmi: number = Number((weight / (height / 100 * height / 100)).toFixed(2));
     let bmiMessage: string = '';
 
     switch (true) {
         case bmi < 18.5:
-            bmiMessage = "Underweight";
+            bmiMessage = `${bmi} - Underweight`;
             break;
 
         case bmi < 25:
-            bmiMessage = "Normal";
+            bmiMessage = `${bmi} - Normal`;
             break;
 
         case bmi < 30:
-            bmiMessage = "Overweight";
+            bmiMessage = `${bmi} - Overweight`;
             break;
 
         default:
-            bmiMessage = "Obese";
+            bmiMessage = `${bmi} - Obese`;
             break;
     }
 
@@ -31,9 +31,11 @@ const calculateBmi = (height: number, weight: number): string => {
 try {
     const heightParameter: number = Number(process.argv[2]);
     const weightParameter: number = Number(process.argv[3]);
-    
+
     console.log(calculateBmi(heightParameter, weightParameter));
 }
 catch (error) {
     console.log(error.message)
 }
+
+export { calculateBmi };
--- part9/calculator/index.ts
+++ part9/calculator/index.ts
@@ -1,14 +1,40 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+
+interface BMI {
+    height: number;
+    weight: number;
+    bmi: string;
+}
 
 const app = express();
 
-app.get('/ping', (_req, res) => {
-    res.send('pong');
-});
-
 app.get('/hello', (_req, res) => res.send('Hello Full Stack!'));
 
-app.get('/', (_req, res) => res.send('Try another endpoint! ;)'));
+app.get('/bmi', (req, res) => {
+    const { height, weight } = req.query;
+
+    // A near on identical check is done internally inside calculateBmi(...)
+    if (!height || !weight) {
+        res.status(400)
+            .send({ error: `Height and weight arguments are required.` })
+    }
+
+    const parsedHeight: number = Number(height)
+    const parsedWeight: number = Number(weight)
+
+    if (isNaN(parsedHeight) || isNaN(parsedWeight)) {
+        res.status(400).send({ error: `Malformatted parameters: try passing both arguments as numbers.` })
+    }
+
+    const bmi: BMI = {
+        weight: parsedWeight,
+        height: parsedHeight,
+        bmi: calculateBmi(parsedHeight, parsedWeight),
+    };
+
+    res.json(bmi);
+});
 
 const PORT = 3003;
 
