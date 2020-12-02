3bd11ef2871f68c414d5999d945c9be1810d7937
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-12-02 07:05:10 +0000

    9.10: Patientor backend, step3
    - endpoint and typed json data provided for diagnoses
    - might end up converting the diagnoses.json to a .ts file...

--- part9/patientor/backend/src/index.ts
+++ part9/patientor/backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosisRouter from './routes/diagnoses';
+
 
 const app = express();
 app.use(express.json());
@@ -11,6 +13,8 @@ app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosisRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
---
+++ part9/patientor/backend/src/routes/diagnoses.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import diagnosisService from '../services/diagnosisService';
+
+const router = express.Router();
+
+router.get('/', (_req, res) => res.send(diagnosisService.getDiagnoses()));
+router.post('/', (_req, res) => res.send('Saving a diary!'));
+
+export default router;
