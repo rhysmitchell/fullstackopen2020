d44d5318a225303a87a3b4c0f10874566cec808f
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-12-03 06:35:37 +0000

    Remove unneeded duplicate file

--- part9/patientor/backend/src/routes/diagnosisService.ts
+++
@@ -1,8 +0,0 @@
-import diagnosisData from '../../data/diagnoses.json';
-import { Diagnosis } from '../types';
-
-const getDiagnoses = (): Diagnosis[] => diagnosisData as Diagnosis[];
-
-export default {
-    getDiagnoses
-};
